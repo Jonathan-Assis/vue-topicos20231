bf45469d1e7766b5474240619e85c2a8ab53a5fb
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-03-20 10:37:10 -0300

    Adição de exemplo de get e post

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,21 +1,63 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
 
-const nome = ref('Nome')
-const contador = ref(1)
+const nome = ref('Nome');
+const senha = ref('123');
+const contador = ref(1);
+const usuarios = ref();
+const erro = ref('');
 
 function incrementar(){
   contador.value++
 }
+async function atualizar() {
+  try {
+    usuarios.value = (await axios.get('usuario')).data;
+    erro.value = '';
+  } catch (e) {
+    erro.value = (e as Error).message;
+  }
+}
+async function cadastrar() {
+  try {
+    await axios.post('usuario', {
+      nome: nome.value,
+      senha:senha.value
+    });
+    erro.value = '';
+    atualizar();
+  } catch (e) {
+    erro.value = (e as Error).message;
+  }
+}
+
+
+onMounted (() => {
+  atualizar();
+}) 
 
 </script>
 <template>
   <div class="about">
     <h1>Bem vindo {{nome}}</h1>
     <p><input type="text" v-model="nome"/></p>
+    <p><input type="password" v-model="senha"/></p>
+    <button @click="cadastrar">Cadastrar</button>
     <p v-if="nome.length > 5">Nome Longo</p>
     <p v-else>Nome curto</p>
     <p>{{ contador }}<button @click="incrementar">Incrementador</button></p>
+    <p v-if="erro">{{ erro }}</p>
+    <table>
+      <thead>
+        <td>Id</td>
+        <td>nome</td>
+      </thead>
+      <tr v-for="(dado) in usuarios" :key="dado.id">
+        <td>{{ dado.id }}</td>
+        <td>{{ dado.nome }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
